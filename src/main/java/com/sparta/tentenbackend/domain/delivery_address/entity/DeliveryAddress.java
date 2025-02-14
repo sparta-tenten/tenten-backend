9c731ea011de45cd9231d12008ec6867d198dc76
sinryuji <siler2474@gmail.com>
2025-02-14 12:16:38 +0900

    feat: DeliveryAddress entity add relation

--- src/main/java/com/sparta/tentenbackend/domain/delivery_address/entity/DeliveryAddress.java
+++ src/main/java/com/sparta/tentenbackend/domain/delivery_address/entity/DeliveryAddress.java
@@ -1,6 +1,8 @@
 package com.sparta.tentenbackend.domain.delivery_address.entity;
 
 import com.sparta.tentenbackend.domain.order.entity.Order;
+import com.sparta.tentenbackend.domain.region.entity.Region;
+import com.sparta.tentenbackend.domain.user.entity.User;
 import com.sparta.tentenbackend.global.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -8,12 +10,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "p_delivery_address")
 public class DeliveryAddress extends BaseEntity {
@@ -28,11 +34,13 @@ public class DeliveryAddress extends BaseEntity {
     @Column(nullable = false)
     private String address;
 
-    @OneToOne(mappedBy = "deliveryAddress", cascade = CascadeType.REMOVE, orphanRemoval = true, optional = false)
+    @OneToOne(mappedBy = "deliveryAddress", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private Order order;
 
-    public DeliveryAddress(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
+    @OneToOne
+    @JoinColumn(name = "region_Id", nullable = false)
+    private Region region;
+
+    @ManyToOne
+    private User user;
 }
