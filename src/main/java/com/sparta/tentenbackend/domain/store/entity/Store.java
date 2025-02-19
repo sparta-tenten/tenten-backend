1c145ad747a909445c9ccc140740852863bd46c3
sinryuji <siler2474@gmail.com>
2025-02-19 16:10:35 +0900

    fix: store constructor

--- src/main/java/com/sparta/tentenbackend/domain/store/entity/Store.java
+++ src/main/java/com/sparta/tentenbackend/domain/store/entity/Store.java
@@ -4,7 +4,6 @@ import com.sparta.tentenbackend.domain.category.entity.Category;
 import com.sparta.tentenbackend.domain.region.entity.Town;
 import com.sparta.tentenbackend.domain.user.entity.User;
 import com.sparta.tentenbackend.global.BaseEntity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,9 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 import java.util.UUID;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,30 +26,40 @@ import lombok.Setter;
 @Table(name = "p_store")
 public class Store extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private UUID id;  // 가게 ID (UUID)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;  // 가게 ID (UUID)
 
-  @Column(nullable = false)
-  private String name;  // 가게명
+    @Column(nullable = false)
+    private String name;  // 가게명
 
-  @Column(nullable = false)
-  private String address;  // 가게 주소
+    @Column(nullable = false)
+    private String address;  // 가게 주소
 
-  @Column(nullable = false)
-  private String phoneNumber;  // 전화번호
+    @Column(nullable = false)
+    private String phoneNumber;  // 전화번호
 
-  private String image;  // 가게 이미지 URL
+    private String image;  // 가게 이미지 URL
 
-  @ManyToOne
-  @JoinColumn(name = "user_id", nullable = false)
-  private User user; // 가게 소유자 (p_user 테이블과 연결)
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // 가게 소유자 (p_user 테이블과 연결)
 
-  @ManyToOne
-  @JoinColumn(name = "category_id", nullable = false)
-  private Category category; // 가게 카테고리 (p_category와 연결)
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category; // 가게 카테고리 (p_category와 연결)
 
-  @ManyToOne
-  @JoinColumn(name = "town_code", nullable = false)
-  private Town town; // 가게 지역 (p_town과 연결)
+    @ManyToOne
+    @JoinColumn(name = "town_code", nullable = false)
+    private Town town; // 가게 지역 (p_town과 연결)
+
+    public Store(String name, String address, String phoneNumber, String image, User user,
+        Category category, Town town) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.image = image;
+        this.user = user;
+        this.category = category;
+    }
 }
