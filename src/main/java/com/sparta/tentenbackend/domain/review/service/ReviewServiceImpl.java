2ae63a855081aadb2c3d7ec20e60bab925188c4c
hanjihae <wlgo32@naver.com>
2025-02-20 20:23:03 +0900

    리뷰 API user 추가, 주석처리 #38

--- src/main/java/com/sparta/tentenbackend/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/sparta/tentenbackend/domain/review/service/ReviewServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sparta.tentenbackend.domain.review.service;
 
+import com.sparta.tentenbackend.domain.order.entity.Order;
+import com.sparta.tentenbackend.domain.order.entity.OrderStatus;
 import com.sparta.tentenbackend.domain.order.repository.OrderRepository;
 import com.sparta.tentenbackend.domain.review.dto.CreateReviewRequestDto;
 import com.sparta.tentenbackend.domain.review.dto.UpdateReviewRequestDto;
 import com.sparta.tentenbackend.domain.review.dto.ReviewResponseDto;
 import com.sparta.tentenbackend.domain.review.entity.Review;
 import com.sparta.tentenbackend.domain.review.repository.ReviewRepository;
+import com.sparta.tentenbackend.domain.user.entity.User;
+import com.sparta.tentenbackend.global.exception.BadRequestException;
+import com.sparta.tentenbackend.global.exception.ForbiddenException;
 import com.sparta.tentenbackend.global.exception.NotFoundException;
 import com.sparta.tentenbackend.global.service.S3Service;
 import java.io.IOException;
@@ -25,61 +30,58 @@ public class ReviewServiceImpl implements ReviewService {
   private final OrderRepository orderRepository;
   private final S3Service s3Service;
 
-  // TODO 리뷰 생성, 수정, 삭제 user 넣기
   // TODO 주문 API 완성되면 주석 없얘고 테스트해보기
-  // 로그인한 유저가 주문한 유저인지 확인
-//  private void confirmYouAreOrderUser(User user, Order order) {
-//    if (!user.getId().equals(order.getUser().getId())) {
-//      throw new ForbiddenException("본인의 주문에 대해서만 리뷰를 작성할 수 있습니다.");
-//    }
-//  }
 
   // 리뷰 만들기
   @Override
   @Transactional
-  public ReviewResponseDto addReview(CreateReviewRequestDto requestDto) throws IOException {
-//    UUID orderId = UUID.fromString(requestDto.getOrderId());
-//    // 주문내역 불러오기
-//    Order order = orderRepository.findById(orderId).orElseThrow(() -> {
-//      throw new NotFoundException("해당 주문을 찾을 수 없습니다.");
-//    });
+  public ReviewResponseDto addReview(CreateReviewRequestDto requestDto, User user) throws IOException {
+    UUID orderId = UUID.fromString(requestDto.getOrderId());
+    // 주문내역 불러오기
+    Order order = orderRepository.findById(orderId).orElseThrow(() -> {
+      throw new NotFoundException("해당 주문을 찾을 수 없습니다.");
+    });
 //    // 로그인한 유저가 주문한 유저인지 확인
-//    confirmYouAreOrderUser(user, order);
-//    // 배달완료인 주문내역인 경우에만 리뷰 작성 가능
-//    if (order.getOrderStatus() != OrderStatus.DELIVERY_COMPLETED) {
-//      throw new BadRequestException("리뷰는 배달완료된 주문에 대해서만 작성할 수 있습니다.");
+//    if (!user.getId().equals(order.getUser().getId())) {
+//      throw new ForbiddenException("본인의 주문에 대해서만 리뷰를 작성할 수 있습니다.");
 //    }
+    // 배달완료인 주문내역인 경우에만 리뷰 작성 가능
+    if (order.getOrderStatus() != OrderStatus.DELIVERY_COMPLETED) {
+      throw new BadRequestException("리뷰는 배달완료된 주문에 대해서만 작성할 수 있습니다.");
+    }
     // 파일이 존재하면 S3에 파일 업로드
     String imageUrl = null;
     if (requestDto.getFile() != null && !requestDto.getFile().isEmpty()) {
       imageUrl = s3Service.uploadFile(requestDto.getFile());
     }
-//    Review review = reviewRepository.save(new Review(requestDto, imageUrl, order));
-    Review review = reviewRepository.save(new Review(requestDto, imageUrl));
+    Review review = reviewRepository.save(new Review(requestDto, imageUrl, order));
     return new ReviewResponseDto(review);
   }
 
   // 리뷰 목록 조회
   @Override
-  public Page<ReviewResponseDto> findAllReviews(int page, int size, String sortBy, boolean isAsc) {
+  public Page<ReviewResponseDto> findAllReviews(User user, int page, int size, String sortBy, boolean isAsc) {
     Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
     Sort sort = Sort.by(direction, sortBy);
     Pageable pageable = PageRequest.of(page, size, sort);
-    // user.getId() == requestDto.getUserId() 확인
-    // Page<Review> reviews = reviewRepository.findAllByUserIdAndIsDeletedFalse(user.getId());
-    Page<Review> reviews = reviewRepository.findAllByIsDeletedFalse(pageable);
+
+    // review 테이블에 저장된 order로 order.getUser().getId() == user.getId() 인 리뷰 목록/isDeleted=false만 조회하기
+    Page<Review> reviews = reviewRepository.findAllByOrder_User_IdAndIsDeletedFalse(user.getId(), pageable);
+//    Page<Review> reviews = reviewRepository.findAllByIsDeletedFalse(pageable);
     return reviews.map(ReviewResponseDto::new);
   }
 
   // 리뷰 수정
   @Override
   @Transactional
-  public ReviewResponseDto modifyReview(UpdateReviewRequestDto requestDto) throws IOException {
-    // user.getId() == requestDto.getUserId() 확인
+  public ReviewResponseDto modifyReview(UpdateReviewRequestDto requestDto, User user) throws IOException {
     Review review = reviewRepository.findById(UUID.fromString(requestDto.getId())).orElseThrow(() ->
         new NotFoundException("해당 리뷰는 존재하지 않습니다."));
+    // 리뷰 작성자 == 주문자 == 로그인 유저인지 확인하기
+//    if (review.getOrder().getUser().getId() != user.getId()) {
+//      throw new ForbiddenException("해당 리뷰의 작성자가 아닙니다.");
+//    }
     // 파일이 존재하면 파일 대체
-    System.out.println(review.getImage());
     String imageUrl = null;
     if (requestDto.getFile() != null && !requestDto.getFile().isEmpty()) {
       imageUrl = s3Service.updateFile(review.getImage(), requestDto.getFile());
@@ -92,13 +94,16 @@ public class ReviewServiceImpl implements ReviewService {
   // 리뷰 삭제
   @Override
   @Transactional
-  public void removeReview(String reviewId) {
-    // user.getId() == requestDto.getUserId() 확인
+  public void removeReview(String reviewId, User user) {
+    // 리뷰 작성자 == 주문자 == 로그인 유저인지 확인하기
+//    if (review.getOrder().getUser().getId() != user.getId()) {
+//      throw new ForbiddenException("해당 리뷰의 작성자가 아닙니다.");
+//    }
     Review review = reviewRepository.findById(UUID.fromString(reviewId)).orElseThrow(() ->
         new NotFoundException("해당 리뷰는 존재하지 않습니다."));
     s3Service.deleteFile(review.getImage());
     review.markAsDeleted();
     reviewRepository.save(review);
   }
-
 }
+
