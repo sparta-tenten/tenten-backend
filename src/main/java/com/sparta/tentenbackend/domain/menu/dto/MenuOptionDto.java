a28e981764719905457c69366da79c303ec43681
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-22 16:02:57 +0900

    Feat: 엔티티를 직접 노축하는 대신, 필요한 필드만 Dto로 생성.

---
+++ src/main/java/com/sparta/tentenbackend/domain/menu/dto/MenuOptionDto.java
@@ -0,0 +1,31 @@
+package com.sparta.tentenbackend.domain.menu.dto;
+
+import com.sparta.tentenbackend.domain.menu.entity.MenuOption;
+import java.math.BigInteger;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MenuOptionDto {
+
+  private UUID id;
+  private String category;
+  private BigInteger price;
+
+  // 생성 메서드 (엔티티 -> DTO)
+  public static MenuOptionDto fromEntity(MenuOption menuOption) {
+    return MenuOptionDto.builder()
+        .id(menuOption.getId())
+        .category(menuOption.getCategory())
+        .price(menuOption.getPrice())
+        .build();
+  }
+}
