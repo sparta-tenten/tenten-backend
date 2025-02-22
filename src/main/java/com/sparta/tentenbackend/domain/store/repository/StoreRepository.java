311448ed91d9c887216c7493246ba3ca83996826
MyYeonbi <yeonbi_kim@icloud.com>
2025-02-23 01:40:23 +0900

    feat: 스토어 도메인에 검색, 정렬 및 페이징 기능 추가

--- src/main/java/com/sparta/tentenbackend/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/tentenbackend/domain/store/repository/StoreRepository.java
@@ -2,6 +2,8 @@ package com.sparta.tentenbackend.domain.store.repository;
 import com.sparta.tentenbackend.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface StoreRepository extends JpaRepository<Store, UUID> {
+public interface StoreRepository extends JpaRepository<Store, UUID>,
+    JpaSpecificationExecutor<Store> {
 }
