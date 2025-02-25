dfec233e78f1298074463c4d21f6c7a58ecc3b3f
sinryuji <siler2474@gmail.com>
2025-02-25 15:33:41 +0900

    fix: get order list

--- src/main/java/com/sparta/tentenbackend/domain/order/dto/OrderOptionResponse.java
+++ src/main/java/com/sparta/tentenbackend/domain/order/dto/OrderOptionResponse.java
@@ -1,6 +1,6 @@
 package com.sparta.tentenbackend.domain.order.dto;
 
-import com.sparta.tentenbackend.domain.menu.entity.MenuOption;
+import com.sparta.tentenbackend.domain.menu.entity.MenuOrderOption;
 import java.util.UUID;
 import lombok.Getter;
 
@@ -11,9 +11,9 @@ public class OrderOptionResponse {
     private final String name;
     private final Long price;
 
-    public OrderOptionResponse(MenuOption menuOption) {
-        this.id = menuOption.getId();
-        this.name = menuOption.getName();
-        this.price = menuOption.getPrice();
+    public OrderOptionResponse(MenuOrderOption op) {
+        this.id = op.getMenuOption().getId();
+        this.name = op.getMenuOption().getName();
+        this.price = op.getMenuOption().getPrice();
     }
 }
