8b1598108ce217bdd9f8f2265505ca902204ec16
sinryuji <siler2474@gmail.com>
2025-02-25 14:25:38 +0900

    fix: 가게 생성 dto

--- src/main/java/com/sparta/tentenbackend/domain/store/dto/StoreRequestDto.java
+++ src/main/java/com/sparta/tentenbackend/domain/store/dto/StoreRequestDto.java
@@ -1,32 +1,28 @@
 package com.sparta.tentenbackend.domain.store.dto;
 
-import com.sparta.tentenbackend.domain.category.entity.Category;
-import com.sparta.tentenbackend.domain.region.entity.Town;
-
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-
 import java.util.UUID;
+import lombok.Getter;
 
 @Getter
 public class StoreRequestDto {
-  @NotBlank(message = "가게 이름은 필수 항목입니다.")
-  private String name;
 
-  @NotBlank(message = "주소는 필수 항목입니다.")
-  private String address;
+    @NotBlank(message = "가게 이름은 필수 항목입니다.")
+    private String name;
 
-  @NotBlank(message = "전화번호는 필수 항목입니다.")
-  private String phoneNumber;
+    @NotBlank(message = "주소는 필수 항목입니다.")
+    private String address;
+
+    @NotBlank(message = "전화번호는 필수 항목입니다.")
+    private String phoneNumber;
 
 
-  private String image;
+    private String image;
 
-  @NotNull(message = "카테고리는 필수 항목입니다.")
-  private UUID categoryId;
+    @NotNull(message = "카테고리는 필수 항목입니다.")
+    private UUID categoryId;
 
-  @NotBlank(message = "동네는 필수 항목입니다.")
-  private Town town;
+    @NotBlank(message = "법정동 코드는 필수 항목입니다.")
+    private String townCode;
 }
